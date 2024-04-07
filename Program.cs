35a1ebc67e041247b8cd640ec7587366f513aebb
felipe <165731375+felipelopes03@users.noreply.github.com>
2024-04-07 03:21:46 -0300

    Dory

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿String Frase;
+
+Console.WriteLine("----Imitando a Dory----");
+
+Console.Write("\nDigite uma Frase:" );
+Frase = Console.ReadLine()!;
+
+String FraseEmBaleis = Frase
+.Replace("a", "aaa") 
+.Replace("A", "AAA") 
+.Replace("e", "eee") 
+.Replace("E", "EEE") 
+.Replace("i", "iii") 
+.Replace("I", "III") 
+.Replace("o", "ooo") 
+.Replace("O", "OOO") 
+.Replace("u", "uuu") 
+.Replace("U", "UUU");
+
+Console.WriteLine($"\nEm baleies:{FraseEmBaleis}");
